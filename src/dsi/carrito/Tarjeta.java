ed1bbc4b626a8e36e94ce6fc8762f0f84a9adf84
sudo-ldasilva <205469926+sudo-ldasilva@users.noreply.github.com>
2026-04-19 17:45:04 -0300

    Agrega constructor de la clase Tarjeta

--- src/dsi/carrito/Tarjeta.java
+++ src/dsi/carrito/Tarjeta.java
@@ -4,4 +4,10 @@ public class Tarjeta {
     private String nombre;
     private MarcaTarjeta marcaTarjeta;
     private String ultimos4digitos;
+
+    public Tarjeta(String nombre, MarcaTarjeta marcaTarjeta, String ultimos4digitos) {
+        this.nombre = nombre;
+        this.marcaTarjeta = marcaTarjeta;
+        this.ultimos4digitos = ultimos4digitos;
+    }
 }
