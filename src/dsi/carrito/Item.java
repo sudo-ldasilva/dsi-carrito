0bed53b0198ef89c79528fd05589297a14f9d216
Andresx2005 <andresvoet10@gmail.com>
2026-04-19 16:16:26 -0300

    Agrego clase Item

--- src/dsi/carrito/Item.java
+++ src/dsi/carrito/Item.java
@@ -1,5 +1,27 @@
 package dsi.carrito;
 
 public class Item {
+    private Carrito carrito;
+    private Producto producto;
+    private int cantidad;
+    private double precioUnitario;
 
+    public Item(Carrito carrito, Producto producto, int cantidad, double precioUnitario) {
+        this.carrito = carrito;
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+    }
+    
+    public double getPrecio(){
+        return cantidad * precioUnitario;
+    }
+    
+    public double getPrecioOficial(){
+        return (cantidad * producto.getPrecio(carrito.getFechaCompra()));
+    }
+    
+    public double getDescuento(){
+        return (this.getPrecioOficial() - this.getPrecio());
+    }
 }
