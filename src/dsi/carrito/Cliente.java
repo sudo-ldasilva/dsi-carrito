3303ec1eea1d2ffc8a1ca49bfbdcfb00e2ac6905
BryanGitman <brygitman@gmail.com>
2026-04-19 16:25:53 -0300

    cliente

--- src/dsi/carrito/Cliente.java
+++ src/dsi/carrito/Cliente.java
@@ -1,5 +1,40 @@
 package dsi.carrito;
 
-public class Cliente {
+import java.util.ArrayList;
 
+public class Cliente {
+    
+    private String nombre;
+    private String apellido;
+    private String email;
+    private ArrayList<Direccion> direcciones;
+    private ArrayList<Carrito> carritos;
+    private ArrayList<Tarjeta> tarjetas;
+    private boolean esPreferencial;
+    
+    public Cliente(String nombre, String apellido, String email, ArrayList<Direccion> direcciones, 
+                    ArrayList<Carrito> carritos, ArrayList<Tarjeta> tarjetas, boolean esPreferencial) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.direcciones = direcciones;
+        this.carritos = carritos;
+        this.tarjetas = tarjetas;
+        this.esPreferencial = esPreferencial;
+    }
+    
+    public double getMontoDeuda() {
+        double montoDeuda = 0;
+        for (Carrito carrito : this.carritos)
+        {
+            montoDeuda += carrito.getMontoDeuda();
+        }
+        return montoDeuda;
+    }
+    
+    public boolean estaHabilitado() {
+        final double LIMITE_DEUDA = 5000000;
+        return this.getMontoDeuda() <= LIMITE_DEUDA;
+    }
+    
 }
