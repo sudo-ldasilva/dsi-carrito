32f9fa5f45afa3cf712bd4bf1710aee65be586cb
ayevitasse <avitasse@frba.utn.edu.ar>
2026-04-19 16:21:07 -0300

    clase de pago

--- src/dsi/carrito/Pago.java
+++ src/dsi/carrito/Pago.java
@@ -1,5 +1,16 @@
 package dsi.carrito;
 
 public class Pago {
-
+    private Carrito carrito; 
+    private Tarjeta tarjeta; 
+    private double monto; 
+    
+    public boolean verificarTarjeta(){
+        return true; 
+    }
+    
+    public double getMonto(){
+        return monto; 
+    }
+    
 }
