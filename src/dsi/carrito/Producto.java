38f6f961afea58c095e012e18c5f4978811a939d
annkizz <antonellajus2005@gmail.com>
2026-04-19 16:27:06 -0300

    clase producto hecha!!

--- src/dsi/carrito/Producto.java
+++ src/dsi/carrito/Producto.java
@@ -1,5 +1,37 @@
 package dsi.carrito;
+import java.util.Date;
+import java.util.ArrayList;
 
 public class Producto {
 
+    private String EAN13;
+    private String nombre;
+    private ArrayList<PrecioProducto> preciosHistoricos = new ArrayList<>();
+    
+    public Producto (String EAN13, String nombre, ArrayList<PrecioProducto> preciosHistoricos) {
+        this.EAN13 = EAN13;
+        this.nombre = nombre;
+        this.preciosHistoricos = preciosHistoricos;
+    }
+    
+    public String getEAN13() {
+        return EAN13;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNombreCorto() {
+        return nombre.substring(0, 4);
+    }
+
+    public double getPrecio(Date fechaVigencia, ArrayList<PrecioProducto> preciosHistoricos) {
+        for (PrecioProducto precio : preciosHistoricos) {
+            if (precio.getCumpleVIgencia(fechaVigencia)) {
+                return precio.getPrecio();
+            }
+        }
+        return 0;
+    }
 }
